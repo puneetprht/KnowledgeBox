7224c25d6f1c132cb72559baddaab0a0d76aedde
puneetprht <puneet_prht@yahoo.co.in>
2022-01-24 02:10:03 +0530

    release related changes

--- setup/App-ReleaseBuild.bat
+++ setup/App-ReleaseBuild.bat
@@ -1,5 +1,7 @@
 ::releaseBuild to automatically build .api
 
+::Change package.json and android/app/build.gradle file for versions
+
 cd C:\Users\Puneet\Documents\Codes\Neeraj\KnowledgeBox\KnowledgeBox\android
 
 call gradlew cleanBuildCache
